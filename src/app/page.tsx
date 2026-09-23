f1ca37ff678fdc081940b95603df3111150bf74f
thuyhong2a5-droid <thuyhong2a5@gmail.com>
2026-09-23 09:52:46 +0700

    update: gioi han so luong san pham ban chay nhat thanh 12 items (3 hang)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function HomePage() {
   const products = localProducts as Product[];
   const collections = localCollections as Collection[];
 
-  const bestSellers = products.filter((p) => p.isBestSeller);
+  const bestSellers = products.filter((p) => p.isBestSeller).slice(0, 12);
   const newArrivals = products.filter((p) => p.isNewArrival);
   const flashSaleProducts = products.filter((p) => p.isSale);
 
